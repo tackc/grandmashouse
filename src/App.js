3c3e953fe17ed9c8609a663c4ba68c7162999dfc
Chris Tack <chris_tack@mac.com>
2019-04-29 21:05:49 -0700

    beginning site layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Alice's Retreat</h1>
+        <hr></hr>
+        <nav className="navigation">
+          <a href="#home" id="home">Home</a>
+          <a href="#rates" id="rates">Rates</a>
+          <a href="#availability" id="availability">Availability</a>
+          <a href="#contact" id="contact">Contact Us</a>
+        </nav>
       </header>
+      <main>
+        <div className="home-page-image"></div>
+        <section>
+          <p>Lorem ipsum dolor sit amet, nam impedit pertinax ut. Sea paulo ludus ut, ut eum justo interesset, alterum accumsan pri et! Ut eos equidem invidunt facilisis. Quem dicit sapientem nec et. Ex quem expetendis duo, ea pri omittam instructior, omnes fabulas ut quo. Liber nostrud at sit, sed populo dolorem urbanitas in, no natum singulis eam.</p>
+          <p>Sit te nihil soleat? Nec in lobortis nominati posidonium. Mei novum voluptatum ea? Pri aperiam adipiscing assueverit ex. Causae dissentias et qui, usu id diceret vocibus. Mei no tation necessitatibus, eos possit ornatus fabellas in, vivendo scribentur dissentiunt te eam.</p>
+        </section>
+      </main>
     </div>
   );
 }
